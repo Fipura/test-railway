8aade28ba8f6b2edcfa0d1da2ce44d04cc30ab60
Pedro Ferreira <116006000+Fipura@users.noreply.github.com>
2024-10-23 20:13:56 +0100

    fix content.json and uncomment dataloader to try and fix issue

--- content-calendar/src/main/java/com/fipura/contentcalendar/config/DataLoader.java
+++ content-calendar/src/main/java/com/fipura/contentcalendar/config/DataLoader.java
@@ -23,9 +23,10 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        if(repository.count() == 0) {
+        if (repository.count() == 0) {
             try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
-                repository.saveAll(objectMapper.readValue(inputStream,new TypeReference<List<Content>>(){}));
+                repository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>() {
+                }));
             }
         }
     }
